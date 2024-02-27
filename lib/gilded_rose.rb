471749d82e7a67a6c7ff1f935db9dc150624834d
Matt McMeans <mrmcmans@memphis.edu>
2024-02-26 21:59:16 -0600

    brie tick refactor

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -64,4 +64,11 @@ class GildedRose
     @quality -= 1
     @quality -= 1 if @days_remaining <= 0
   end
+
+  def brie_tick
+    @days_remaining -= 1
+    return if @quality >= 50
+    @quality += 1
+    @quality += 1 if @days_remaining <= 0
+  end
 end
