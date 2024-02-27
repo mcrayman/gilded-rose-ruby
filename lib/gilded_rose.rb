252dff3f480cd315516abcdb29074e9d50247d22
Matt McMeans <mrmcmans@memphis.edu>
2024-02-27 14:13:09 -0600

    rewind

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,13 +10,17 @@ class GildedRose
   def tick
     case name
     when 'Normal Item'
-      return normal_tick
+      @item = Normal.new(quality, days_remaining)
+      item.tick
     when 'Aged Brie'
-      return brie_tick
+      @item = Brie.new(quality, days_remaining)
+      item.tick
     when 'Sulfuras, Hand of Ragnaros'
-      return sulfuras_tick
+      @item = Sulfuras.new(quality, days_remaining)
+      item.tick
     when 'Backstage passes to a TAFKAL80ETC concert'
-      return backstage_tick
+      @item = Backstage.new(quality, days_remaining)
+      item.tick
     end
   end
 
